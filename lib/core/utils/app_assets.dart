a9585076233614f960f50ae0bcb56754ebacf0a9
salsabilelshiekh2022 <salsabilelshiekh6@gmail.com>
2024-04-15 20:57:13 +0200

    -Add images

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,20 @@
+abstract class AppAssets {
+  static const String avatar = 'assets/images/png/avatar.png';
+  static const String birthday = 'assets/images/png/birthday.png';
+  static const String congratulation = 'assets/images/png/congratulation.png';
+  static const String congratulation2 = 'assets/images/png/congratulation2.png';
+  static const String createNewPass = 'assets/images/png/create_new_pass.png';
+  static const String forgotPass = 'assets/images/png/forgot_pass.png';
+  static const String onboarding1 = 'assets/images/png/onboarding1.png';
+  static const String onboarding2 = 'assets/images/png/onboarding2.png';
+  static const String onboarding3 = 'assets/images/png/onboarding3.png';
+  static const String welcomeImage = 'assets/images/png/welcome_image.png';
+
+  static const String facebookIcon = 'assets/images/svg/facebook_icon.svg';
+  static const String femaleIcon = 'assets/images/svg/female_icon.svg';
+  static const String googleIcon = 'assets/images/svg/google_icon.svg';
+  static const String logo = 'assets/images/svg/logo.svg';
+  static const String mailIcon = 'assets/images/svg/mail_icon.svg';
+  static const String maleIcon = 'assets/images/svg/male_icon.svg';
+  static const String messageIcon = 'assets/images/svg/message_icon.svg';
+}
