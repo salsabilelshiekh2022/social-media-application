7578ba515b9427f2ac0c5f01fa09be2a12c9d00b
salsabilelshiekh2022 <salsabilelshiekh6@gmail.com>
2024-04-15 20:22:17 +0200

    -Create snack bar

---
+++ lib/core/utils/snack_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'app_constants.dart';
+
+void showSnackBar(String msg, Color color) {
+  final SnackBar snackBar = SnackBar(
+    content: Text(
+      msg,
+      style: const TextStyle(fontSize: 16),
+      textAlign: TextAlign.center,
+      maxLines: 2,
+      overflow: TextOverflow.ellipsis,
+    ),
+    duration: const Duration(seconds: 1),
+    backgroundColor: color,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+  );
+
+  scaffoldKey.currentState?.showSnackBar(snackBar);
+}
